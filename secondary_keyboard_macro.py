f8f846b745d2387a02f90a5a7a16a5cfb614f340
Nemanja Jovic <nemanja.jovic195@gmail.com>
2025-06-04 12:31:44 +0200

    Added system tray icon with Quit Program functionality

--- secondary_keyboard_macro.py
+++ secondary_keyboard_macro.py
@@ -1,10 +1,13 @@
 import os
 import time
+from threading import Thread
 
 import keyboard
 import pyautogui
 import pygetwindow
 import pyperclip
+import pystray
+from PIL import Image, ImageDraw
 
 from clipboard_modifier import modify_copied_content
 
@@ -12,7 +15,7 @@ from clipboard_modifier import modify_copied_content
 target_variable = "local keyboardIdentifier"
 
 
-# modify_copied_content takes this as a second parameter
+# Keyboard selection will be added later to the tkinter GUI so we can select the keyboard we want
 class KeyboardSelection:
     work_keyboard = "'2FE3714A'"
     home_keyboard = "'14C79465'"
@@ -21,9 +24,10 @@ class KeyboardSelection:
 def start_lua():
     with open("./luascript.lua", "r") as file:
         pyperclip.copy(file.read())
+    # Change KeyboardSelection to either home_keyboard or work_keyboard
     modify_copied_content(target_variable, KeyboardSelection.home_keyboard)
     os.startfile("C://Users/User/Desktop/luamacros/LuaMacros.exe")
-    time.sleep(1.5)
+    time.sleep(2)
     pyautogui.hotkey("ctrl", "v")
     pyautogui.click(473, 169)
     pygetwindow.getActiveWindow().minimize()
@@ -31,11 +35,9 @@ def start_lua():
 
 def close_program():
     os.system("taskkill /f /im luamacros.exe")
-    print("Closing the python script...")
+    print("Closing the Python script...")
     time.sleep(1)
-    print(
-        "SUCCESS: Python process has been terminated"
-    )  # Lets make our console a little pretty shall we?
+    print("SUCCESS: Python process has been terminated")
     os._exit(0)
 
 
@@ -49,7 +51,7 @@ def read_file():
         with open(file_path, "r") as file:
             content = file.read().strip()
             if content == "k":
-                close_program()  # this kills python and lua
+                close_program()  # I will not keep this as a macro since I have a tray icon to kill this program, leaving for testing purposes for now
             elif content == "b":
                 os.startfile(
                     "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Brave.lnk"
@@ -68,4 +70,17 @@ def on_press(event):
 keyboard.on_press(on_press)
 
 print("Listening for macro key press...")
+
+
+def create_icon():
+    icon_image = Image.open("./res/icon.png")
+    menu = (pystray.MenuItem("Exit", close_program),)
+    icon = pystray.Icon("LuaMacros", icon_image, "LuaMacros Manager", menu)
+    icon.run()
+
+
+# Start tray icon in a separate thread
+tray_thread = Thread(target=create_icon, daemon=True)
+tray_thread.start()
+
 keyboard.wait()
